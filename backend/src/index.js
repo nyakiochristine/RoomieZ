299984a8c768ce275de79155db7f058eef5dade8
christine <christine.mwangi@student.moringaschool.com>
2026-03-05 11:44:57 +0300

    ft:added calls

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const http = require('http'); // 1. Import HTTP module
 const { Server } = require('socket.io'); // 2. Import Socket.io
@@ -25,8 +26,10 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 8000;
 
 // Middleware
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 // Use Routes
 app.use('/api/auth', authRoutes);
